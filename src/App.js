69f1cf31c438b786c6e1cfdf96c5474a690b5597
Angelishere <angelsara1804@gmail.com>
2024-04-19 22:08:27 +0530

    Search product added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddProducts from './components/AddProducts';
+import SearchProduct from './components/SearchProduct';
 
 function App() {
   return (
     <div>
-    <AddProducts/>
-
+      <AddProducts />
+      <SearchProduct />
 
     </div>
   );
