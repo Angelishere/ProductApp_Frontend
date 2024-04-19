704276c98c8ec8ec3dddf662c185afa65d21d28b
Angelishere <angelsara1804@gmail.com>
2024-04-19 22:39:48 +0530

    View product added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddProducts from './components/AddProducts';
 import SearchProduct from './components/SearchProduct';
+import ViewProducts from './components/ViewProducts';
 
 function App() {
   return (
     <div>
       <AddProducts />
       <SearchProduct />
+      <ViewProducts />
 
     </div>
   );
